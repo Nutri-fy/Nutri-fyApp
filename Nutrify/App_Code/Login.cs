835cf95060989055df5ffa07066a8e24ed777301
Deemantha-Silva <deemantha.silva@bell.net>
2018-07-19 23:42:00 -0400

    fixed to not show which login field was incorrect for security reasons

--- Nutrify/App_Code/Login.cs
+++ Nutrify/App_Code/Login.cs
@@ -13,7 +13,6 @@ public class Login
     private string conString = "Server=den1.mssql2.gear.host; Database=class2018; User=class2018; Password=c#class";
     private SqlCommand cmd;
     public int userID;
-    public string[] loginInfo = new string[2];
     
     public bool isLogin(string uname, string pass)
     {
@@ -30,28 +29,10 @@ public class Login
             string result = cmd.ExecuteScalar().ToString();
             if (result=="1")
             {
-                string queryUserID = "SELECT userID from UserInfo where userName like '" + uname + "' AND password like '" + pass + "';";
-                cmd.CommandText = queryUserID;
-                userID = Convert.ToInt32(cmd.ExecuteScalar());
-
-                string queryUserName = "SELECT userName from UserInfo where userName like '" + uname + "';";
-                cmd.CommandText = queryUserName;
-                loginInfo[0] = cmd.ExecuteScalar().ToString();
-
-                string queryPassword = "SELECT password from UserInfo where password like '" + pass + "';";
-                cmd.CommandText = queryPassword;
-                loginInfo[1] = cmd.ExecuteScalar().ToString();
                 return true;
             }
             else
             {
-                string queryUserName= "SELECT userName from UserInfo where userName like '" + uname + "';";
-                cmd.CommandText = queryUserName;
-                loginInfo[0] = cmd.ExecuteScalar().ToString();
-
-                string queryPassword = "SELECT password from UserInfo where password like '" + pass + "';";
-                cmd.CommandText = queryPassword;
-                loginInfo[1] = cmd.ExecuteScalar().ToString();
                 return false;
             }
         }catch (Exception e)
@@ -69,15 +50,5 @@ public class Login
     {
         return userID;
     }
-
-    public string[] getLoginInfo()
-    {
-        return loginInfo;
-    }
-
-
-
-
-
 
 }
