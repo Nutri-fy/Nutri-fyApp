54b341ec98cc7643f57c94a0f6fba0a24c0d010b
joe kennedy <joe_4114@hotmail.com>
2018-08-07 17:37:17 -0400

    I cant get the select * to show data sorry

---
+++ Nutrify/UserCon.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="UserCon.aspx.cs" Inherits="UserCon" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    
+    <asp:Label ID="lblUserName" runat="server" Text="Label"></asp:Label>
+    <br />
+    <div>
+        <asp:Label ID="Label1" runat="server" Text="First Name"></asp:Label>
+&nbsp;</div>
+    <asp:TextBox ID="tbFirstName" runat="server"></asp:TextBox>
+
+&nbsp;<br />
+      <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ControlToValidate="tbFirstName" ErrorMessage="Please enter your First Name" ForeColor="Red" Display="Dynamic"></asp:RequiredFieldValidator>
+
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="Last Name"></asp:Label>
+    <br />
+    <asp:TextBox ID="tbLastName" runat="server"></asp:TextBox>
+
+    &nbsp;<br />
+    &nbsp;<asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" ControlToValidate="tbLastName" ErrorMessage="Please enter your Last Name" ForeColor="Red" Display="Dynamic"></asp:RequiredFieldValidator>
+
+    <br />
+   
+    <div>
+        <asp:Label ID="Label3" runat="server" Text="Password"></asp:Label>
+&nbsp;&nbsp; 
+    &nbsp;
+    </div>
+    <asp:TextBox ID="tbPassword1" runat="server"></asp:TextBox>
+&nbsp;<br />
+      <asp:RequiredFieldValidator ID="ReqPass" runat="server" ControlToValidate="tbPassword1" ErrorMessage="Please enter password" ForeColor="Red" Display="Dynamic"></asp:RequiredFieldValidator>
+      <br />
+    <asp:Label ID="Label4" runat="server" Text="Re-enter Password"></asp:Label>
+    <br />
+    <asp:TextBox ID="tbPassword2" runat="server"></asp:TextBox>
+    &nbsp;<br />
+      <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="tbPassword2" ErrorMessage="Please enter password" ForeColor="Red" Display="Dynamic"></asp:RequiredFieldValidator>
+      <asp:CompareValidator ID="ComparePass" runat="server" ControlToValidate="tbPassword2" ControlToCompare="tbPassword1" ErrorMessage="Passwords do not match" ForeColor="Red" Display="Dynamic"></asp:CompareValidator>
+    <br />
+    <div>
+        <asp:Label ID="Label5" runat="server" Text="Email"></asp:Label>
+&nbsp;&nbsp;&nbsp; 
+    &nbsp;
+    </div>
+    <asp:TextBox ID="tbEmail" runat="server"></asp:TextBox>
+    <br />
+    <asp:RequiredFieldValidator ID="ReqEmail" runat="server" ControlToValidate="tbEmail" Display="Dynamic" ErrorMessage="Please enter your Email" ForeColor="Red"></asp:RequiredFieldValidator>
+    <asp:RegularExpressionValidator ID="regEmail" runat="server" ControlToValidate="tbEmail" Display="Dynamic" ErrorMessage="Not a valid Email!" ForeColor="Red" ValidationExpression="\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*"  ></asp:RegularExpressionValidator>
+    <br />
+    <asp:Label ID="Label6" runat="server" Text="Age"></asp:Label>
+    <br />
+&nbsp;<asp:TextBox ID="tbAge" runat="server"></asp:TextBox>
+    &nbsp;<br />
+&nbsp;&nbsp;<asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="tbAge" Display="Dynamic" ErrorMessage="Please enter your Age" ForeColor="Red"></asp:RequiredFieldValidator>
+    <br />
+    <br />
+    <asp:Button ID="btnSave" runat="server" class="btn btn-primary" OnClick="btnSave_Click" Text="Save Update" />
+    <br />
+    <asp:Button ID="btnCancel" runat="server" class="btn btn-primary" OnClick="btnCancel_Click" Text="Cancel" />
+    
+</asp:Content>
+
---
+++ Nutrify/UserCon.aspx.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class UserCon : System.Web.UI.Page
+{
+    SqlConnection UserConnect = new SqlConnection();
+    string connString = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["Nutri-fyConnectionString"].ConnectionString;
+    SqlCommand cmd;
+    string userName;
+    string firstName;
+    string lastName;
+    string password;
+    string email;
+    int age;
+    int isAdmin;
+    int gender;
+    double weight;
+    double height;
+    double activity;
+    double calories;
+    int numOfMeals;
+    int goal;
+    double prPro;
+    double prCarb;
+    double prFats;
+    int uId;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        UserConnect.ConnectionString = connString;
+        cmd = UserConnect.CreateCommand();
+        UserConnect.Open();
+        
+        uId = Convert.ToInt32(Session["sUserId"]);
+        try
+        {
+            string query = "SELECT firstName, lastName from UserInfo where userID like'" + 3 + "' ;";
+            cmd.CommandText = query;
+            var reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    firstName = reader[0].ToString();
+                    lastName = reader[1].ToString();
+                }
+            }
+        }
+        catch
+        {
+
+        }
+        finally
+        {
+            cmd.Dispose();
+            UserConnect.Close();
+        }
+        lblUserName.Text = "Welcome " + firstName + " " + lastName;
+        try
+        {           
+            string query = "SELECT * from UserInfo where userID like'" + 3 + "' ;";
+            cmd.CommandText = query;
+            var reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    userName = reader[1].ToString();
+                    password = reader[2].ToString();
+                    firstName = reader[3].ToString();
+                    lastName = reader[4].ToString();
+                    email = reader[5].ToString();
+                    isAdmin = Convert.ToInt32(reader[6]);
+                    age = Convert.ToInt32(reader[7]);
+                    gender = Convert.ToInt32(reader[8]);
+                    height = Convert.ToInt32(reader[9]);
+                    weight = Convert.ToDouble(reader[10]);
+                    activity = Convert.ToDouble(reader[11]);
+                    calories = Convert.ToDouble(reader[12]);
+                    numOfMeals = Convert.ToInt32(reader[13]);
+                    goal = Convert.ToInt32(reader[14]);
+                    prPro = Convert.ToDouble(reader[15]);
+                    prCarb = Convert.ToDouble(reader[16]);
+                    prFats = Convert.ToDouble(reader[17]);
+
+                }
+            }
+            tbFirstName.Text = firstName;
+            tbLastName.Text = lastName;
+            tbPassword1.Text = password;
+            tbPassword2.Text = password;
+            tbEmail.Text = email;
+            tbAge.Text = age.ToString();
+        }
+        catch
+        {
+
+        }
+        finally
+        {
+            cmd.Dispose();
+            UserConnect.Close();
+        }
+
+    }
+
+
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            string query = "UPDATE FlightInfo set userName=@userName,password=@password,firstName=@firstName,lastName=@lastName,email=@email,isAdmin=@isAdmin," +
+            "age=@age,gender=@gender,height=@height,weight=@weight,activity=@activity,calories=@calories,numOfMeals=@numOfMeals,goal=@goal,prPro=@prPro," +
+            "prCarb=@prCarb,prFats=@prFats where userID=@userID;";
+
+            SqlParameter userNamepar = new SqlParameter("@userName", userName);
+            SqlParameter passpar = new SqlParameter("@password", tbPassword2.Text);
+            SqlParameter firstNamepar = new SqlParameter("@firstName", tbFirstName.Text);
+            SqlParameter lastNamepar = new SqlParameter("@lastName", tbLastName.Text);
+            SqlParameter emailpar = new SqlParameter("@email", tbEmail.Text);
+            SqlParameter isAdminpar = new SqlParameter("@isAdmin", isAdmin);
+            SqlParameter agepar = new SqlParameter("@age", tbAge.Text);
+            SqlParameter genderpar = new SqlParameter("@gender", gender);
+            SqlParameter heightpar = new SqlParameter("@height", height);
+            SqlParameter weightpar = new SqlParameter("@weight", height);
+            SqlParameter activitypar = new SqlParameter("@activity", activity);
+            SqlParameter calpar = new SqlParameter("@calories", calories);
+            SqlParameter numOfMealspar = new SqlParameter("@numOfMeals", numOfMeals);
+            SqlParameter goalpar = new SqlParameter("@goal", goal);
+            SqlParameter prPropar = new SqlParameter("@prPro", prPro);
+            SqlParameter prCarbpar = new SqlParameter("@prCarb", prCarb);
+            SqlParameter prFatspar = new SqlParameter("@prFats", prFats);
+            SqlParameter IDpar = new SqlParameter("@userID", uId);
+
+            cmd.CommandText = query;
+            cmd.Parameters.Add(userNamepar);
+            cmd.Parameters.Add(passpar);
+            cmd.Parameters.Add(firstNamepar);
+            cmd.Parameters.Add(lastNamepar);
+            cmd.Parameters.Add(emailpar);
+            cmd.Parameters.Add(isAdminpar);
+            cmd.Parameters.Add(agepar);
+            cmd.Parameters.Add(genderpar);
+            cmd.Parameters.Add(heightpar);
+            cmd.Parameters.Add(weightpar);
+            cmd.Parameters.Add(activitypar);
+            cmd.Parameters.Add(calpar);
+            cmd.Parameters.Add(numOfMealspar);
+            cmd.Parameters.Add(goalpar);
+            cmd.Parameters.Add(prPropar);
+            cmd.Parameters.Add(prCarbpar);
+            cmd.Parameters.Add(prFatspar);
+            cmd.Parameters.Add(IDpar);
+
+            UserConnect.Open();
+        }
+        catch
+        {
+
+        }
+        finally
+        {
+            cmd.Dispose();
+            UserConnect.Close();
+        }
+    }
+
+    protected void btnCancel_Click(object sender, EventArgs e)
+    {
+        tbAge.Text = "";
+        tbEmail.Text = "";
+        tbFirstName.Text = "";
+        tbLastName.Text = "";
+        tbPassword1.Text = "";
+        tbPassword2.Text = "";
+    }
+}
