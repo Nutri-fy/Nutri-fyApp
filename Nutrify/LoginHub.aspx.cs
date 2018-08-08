7061b3f7a411c28eab2d1e756a1d10808242bee0
Adil Hassan <adilh244@gmail.com>
2018-08-08 01:23:51 -0400

    realtime date tim for hub working. Button to redirect to calcount

---
+++ Nutrify/LoginHub.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Timers;
+
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Label1.Text = DateTime.Now.ToLongDateString();
+        Label2.Text = DateTime.Now.Hour.ToString();
+        Label2.Text += ":" + DateTime.Now.Minute.ToString();
+        Label2.Text += ":" + DateTime.Now.Second.ToString();
+    }
+
+
+
+    protected void btnStat_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("CalCount.aspx");
+    }
+}
+
+
+
+
+
+
+
+
+    
